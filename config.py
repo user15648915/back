aa7a2008922c2abe79564c613b21c556d040eb4f
Dastan <dastan161206@gmail.com>
2025-12-07 22:36:24 +0500

    Update API_URL to use Render public address

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ load_dotenv("b.env")
 
 class Config:
     # 🔑 Flask secret key (для сессий, CSRF и пр.)
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
     # 🔌 Подключение к базе данных PostgreSQL
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or 'postgresql://user:password@localhost/flashcards_db'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # 🔐 Настройки JWT
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwtsecretkey")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=30)  # срок жизни токена (30 дней)
 
     # 🔧 Дополнительно (можно включить при необходимости)
